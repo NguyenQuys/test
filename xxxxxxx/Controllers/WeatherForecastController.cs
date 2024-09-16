5499a4b73482ec1d1614e64c6ffdb65c731c8a28
NguyenQuys <nguyenhuuquy110203@gmail.com>
2024-09-16 21:25:34 +0700

    mkmkkmminti2

--- xxxxxxx/Controllers/WeatherForecastController.cs
+++ xxxxxxx/Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ namespace xxxxxxx.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching","quytest","aaaaaaaaa"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
